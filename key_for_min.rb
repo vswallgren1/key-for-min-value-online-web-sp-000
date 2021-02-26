e265129089d091685df26035de21cebff95d3468
Vince W <vincewallgren@me.com>
2021-02-26 05:05:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(stadiums)
+def key_for_min_value(name_hash)
   stadiums = {
     :napoli => 80000,
     :milan => 76000,
