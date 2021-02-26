808ca48782c824f8e4fdf4dd3cd2cc0c618bc895
Vince W <vincewallgren@me.com>
2021-02-26 05:07:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  stadiums = {
-    :napoli => 80000,
-    :milan => 76000,
-    :juventus => 43000
-  }
-    stadiums.each do |club, capacity|
-
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
